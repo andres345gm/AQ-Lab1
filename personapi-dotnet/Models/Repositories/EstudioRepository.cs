804c393199bcf07b1532f7cd33b0b82ba63759b1
RhaxCity <juanjogomezarenas1@gmail.com>
2024-10-07 08:57:46 -0500

    Initial commit

---
+++ personapi-dotnet/Models/Repositories/EstudioRepository.cs
@@ -0,0 +1,49 @@
+﻿using personapi_dotnet.Models.Entities;
+using personapi_dotnet.Models.Interfaces;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace personapi_dotnet.Models.Repositories
+{
+    public class EstudioRepository : IEstudioRepository
+    {
+        private readonly PersonaDbContext _context;
+
+        public EstudioRepository(PersonaDbContext context)
+        {
+            _context = context;
+        }
+
+        public IEnumerable<Estudio> GetAll()
+        {
+            return _context.Estudios.ToList();
+        }
+
+        public Estudio GetById(int id_prof, int cc_per)
+        {
+            return _context.Estudios.Find(id_prof, cc_per);
+        }
+
+        public void Add(Estudio estudio)
+        {
+            _context.Estudios.Add(estudio);
+            _context.SaveChanges();
+        }
+
+        public void Update(Estudio estudio)
+        {
+            _context.Estudios.Update(estudio);
+            _context.SaveChanges();
+        }
+
+        public void Delete(int id_prof, int cc_per)
+        {
+            var estudio = _context.Estudios.Find(id_prof, cc_per);
+            if (estudio != null)
+            {
+                _context.Estudios.Remove(estudio);
+                _context.SaveChanges();
+            }
+        }
+    }
+}
---
+++ personapi-dotnet/Models/Repositories/ProfesionRepository.cs
@@ -0,0 +1,51 @@
+﻿using personapi_dotnet.Models.Entities;
+using personapi_dotnet.Models.Interfaces;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace personapi_dotnet.Models.Repositories
+{
+    public class ProfesionRepository : IProfesionRepository
+    {
+        private readonly PersonaDbContext _context;
+
+        public ProfesionRepository(PersonaDbContext context)
+        {
+            _context = context;
+        }
+
+        public IEnumerable<Profesion> GetAll()
+        {
+            // Utiliza la propiedad DbSet de Profesion
+            return _context.Profesions.ToList(); // Asegúrate de usar "Profesions"
+        }
+
+        public Profesion GetById(int id)
+        {
+            return _context.Profesions.Find(id);
+        }
+
+        public void Add(Profesion profesion)
+        {
+            _context.Profesions.Add(profesion);
+            _context.SaveChanges();
+        }
+
+        public void Update(Profesion profesion)
+        {
+            _context.Profesions.Update(profesion);
+            _context.SaveChanges();
+        }
+
+        public void Delete(int id)
+        {
+            var profesion = _context.Profesions.Find(id);
+            if (profesion != null)
+            {
+                _context.Profesions.Remove(profesion);
+                _context.SaveChanges();
+            }
+        }
+    }
+}
