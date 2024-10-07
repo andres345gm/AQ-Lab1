277d018226de3523a944d5750c8a87def3826f96
Andrés García Montoya <61592522+andres345gm@users.noreply.github.com>
2024-10-07 17:45:20 -0500

    Changes in db

--- personapi-dotnet/init.sql
+++ personapi-dotnet/init.sql
@@ -1,81 +1,60 @@
--- MySQL Workbench Forward Engineering
+-- Crear la base de datos arq_per_db
+IF NOT EXISTS (SELECT * FROM sys.databases WHERE name = 'arq_per_db')
+BEGIN
+    CREATE DATABASE arq_per_db;
+END
+GO
 
-SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
-SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
-SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION';
+USE arq_per_db;
+GO
 
--- -----------------------------------------------------
--- Schema arq_per_db
--- -----------------------------------------------------
+-- Crear la tabla persona
+IF NOT EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'[dbo].[persona]') AND type in (N'U'))
+BEGIN
+    CREATE TABLE persona (
+        cc INT NOT NULL PRIMARY KEY,
+        nombre NVARCHAR(45) NOT NULL,
+        apellido NVARCHAR(45) NOT NULL,
+        genero CHAR(1) NOT NULL CHECK (genero IN ('M', 'F')), -- Usamos CHAR(1) para el genero
+        edad INT NULL
+    );
+END
+GO
 
--- -----------------------------------------------------
--- Schema arq_per_db
--- -----------------------------------------------------
-CREATE SCHEMA IF NOT EXISTS `arq_per_db` DEFAULT CHARACTER SET latin1 ;
-USE `arq_per_db` ;
+-- Crear la tabla profesion
+IF NOT EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'[dbo].[profesion]') AND type in (N'U'))
+BEGIN
+    CREATE TABLE profesion (
+        id INT NOT NULL PRIMARY KEY,
+        nom NVARCHAR(90) NOT NULL,
+        des NVARCHAR(MAX) NULL -- NVARCHAR(MAX) para el campo TEXT en SQL Server
+    );
+END
+GO
 
--- -----------------------------------------------------
--- Table `arq_per_db`.`persona`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `arq_per_db`.`persona` (
-  `cc` INT(15) NOT NULL,
-  `nombre` VARCHAR(45) NOT NULL,
-  `apellido` VARCHAR(45) NOT NULL,
-  `genero` ENUM('M', 'F') NOT NULL,
-  `edad` INT(3) NULL DEFAULT NULL,
-  PRIMARY KEY (`cc`))
-ENGINE = InnoDB
-DEFAULT CHARACTER SET = latin1;
+-- Crear la tabla estudios
+IF NOT EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'[dbo].[estudios]') AND type in (N'U'))
+BEGIN
+    CREATE TABLE estudios (
+        id_prof INT NOT NULL,
+        cc_per INT NOT NULL,
+        fecha DATE NULL,
+        univer NVARCHAR(50) NULL,
+        PRIMARY KEY (id_prof, cc_per),
+        FOREIGN KEY (cc_per) REFERENCES persona(cc),
+        FOREIGN KEY (id_prof) REFERENCES profesion(id)
+    );
+END
+GO
 
-
--- -----------------------------------------------------
--- Table `arq_per_db`.`profesion`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `arq_per_db`.`profesion` (
-  `id` INT(6) NOT NULL,
-  `nom` VARCHAR(90) NOT NULL,
-  `des` TEXT NULL DEFAULT NULL,
-  PRIMARY KEY (`id`))
-ENGINE = InnoDB
-DEFAULT CHARACTER SET = latin1;
-
-
--- -----------------------------------------------------
--- Table `arq_per_db`.`estudios`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `arq_per_db`.`estudios` (
-  `id_prof` INT(6) NOT NULL,
-  `cc_per` INT(15) NOT NULL,
-  `fecha` DATE NULL DEFAULT NULL,
-  `univer` VARCHAR(50) NULL DEFAULT NULL,
-  PRIMARY KEY (`id_prof`, `cc_per`),
-  INDEX `estudio_persona_fk` (`cc_per` ASC) VISIBLE,
-  CONSTRAINT `estudio_persona_fk`
-    FOREIGN KEY (`cc_per`)
-    REFERENCES `arq_per_db`.`persona` (`cc`),
-  CONSTRAINT `estudio_profesion_fk`
-    FOREIGN KEY (`id_prof`)
-    REFERENCES `arq_per_db`.`profesion` (`id`))
-ENGINE = InnoDB
-DEFAULT CHARACTER SET = latin1;
-
-
--- -----------------------------------------------------
--- Table `arq_per_db`.`telefono`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `arq_per_db`.`telefono` (
-  `num` VARCHAR(15) NOT NULL,
-  `oper` VARCHAR(45) NOT NULL,
-  `duenio` INT(15) NOT NULL,
-  PRIMARY KEY (`num`),
-  INDEX `telefono_persona_fk` (`duenio` ASC) VISIBLE,
-  CONSTRAINT `telefono_persona_fk`
-    FOREIGN KEY (`duenio`)
-    REFERENCES `arq_per_db`.`persona` (`cc`))
-ENGINE = InnoDB
-DEFAULT CHARACTER SET = latin1;
-
-
-SET SQL_MODE=@OLD_SQL_MODE;
-SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
-SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;
+-- Crear la tabla telefono
+IF NOT EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'[dbo].[telefono]') AND type in (N'U'))
+BEGIN
+    CREATE TABLE telefono (
+        num NVARCHAR(15) NOT NULL PRIMARY KEY,
+        oper NVARCHAR(45) NOT NULL,
+        duenio INT NOT NULL,
+        FOREIGN KEY (duenio) REFERENCES persona(cc)
+    );
+END
+GO
